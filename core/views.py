596363f8c1f134e334aa31661678d2050cd64b9e
Diego Alves <diegomoal@gmail.com>
2021-02-17 20:08:04 -0300

    Add room

--- core/views.py
+++ core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'sala': '35',
+        'predio': 'B'
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
