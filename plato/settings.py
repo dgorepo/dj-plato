ddf23a42453718167efa14b1a4b72edee9489479
Diego Alves <diegomoal@gmail.com>
2021-02-17 13:29:21 -0300

    Alterada localizacao

--- plato/settings.py
+++ plato/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$v3#0eud4sd=6w36%73a$!01)tdvu6ose5ybosutj6#vc9qhcc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
